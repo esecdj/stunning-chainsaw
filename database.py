2cd2b570c23d537bdddbb20479acd44b039a2b6c
esecdj <145449162+esecdj@users.noreply.github.com>
2025-07-22 14:40:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-jdbc = os.getenv("DATABASE_URL")
-print(jdbc)
+password = os.getenv("DATABASE_URL")
+print(password)
 engine = create_async_engine(DATABASE_URL, echo=True)
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
